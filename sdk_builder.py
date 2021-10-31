249d015dbe47b74c330151896b1e0ac26234cd01
kat <KritantaDevelopment@gmail.com>
2021-10-31 02:18:36 -0400

    Actually sort the methods🤦‍♀️

--- sdk_builder.py
+++ sdk_builder.py
@@ -25,6 +25,17 @@ def dump(fold, fw_name):
     machofile = MachOFile(fd)
     library = Dyld.load(machofile.slices[0])
     objc_lib = ObjCLibrary(library)
+    
+    
+    for objc_class in objc_lib.classlist:
+        objc_class.methods.sort(key=lambda h: h.signature)
+        objc_class.properties.sort(key=lambda h: h.name)
+        if objc_class.metaclass is not None:
+            objc_class.metaclass.methods.sort(key=lambda h: h.signature)
+
+    for objc_proto in objc_lib.protolist:
+        objc_proto.methods.sort(key=lambda h: h.signature)
+        objc_proto.opt_methods.sort(key=lambda h: h.signature)
 
     hg = HeaderGenerator(objc_lib)
     os.makedirs(f'{working_dir}/System/Library/{fold}/{fw_name}.framework', exist_ok=True)
@@ -35,11 +46,6 @@ def dump(fold, fw_name):
         tbd_out.write(TapiYAMLWriter.write_out(tbd_dict))
         
     os.makedirs(f'{working_dir}/System/Library/{fold}/{fw_name}.framework/Headers', exist_ok=True)
-    
-    for objc_class in objc_lib.classlist:
-        objc_class.methods = sorted(objc_class.methods, key=lambda h: h.signature)
-        objc_class.properties = sorted(objc_class.properties, key=lambda h: h.name)
-
     
     for header_name in hg.headers:
         with open(f'{working_dir}/System/Library/{fold}/{fw_name}.framework/Headers' + '/' + header_name,
