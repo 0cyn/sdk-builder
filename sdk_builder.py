8b81cf5f56fc0581c7c59c8f4ffccde333bc9626
kat <KritantaDevelopment@gmail.com>
2021-12-04 21:41:25 -0500

    Update sdk_builder.py

--- sdk_builder.py
+++ sdk_builder.py
@@ -5,12 +5,7 @@
 import os
 import time
 import concurrent.futures
-from ktool.macho import MachOFile
-from ktool.objc import ObjCImage, LinkedClass
-from ktool.dyld import Dyld
-from ktool.generator import TBDGenerator
-from ktool.headers import HeaderGenerator
-from ktool.util import TapiYAMLWriter
+import ktool
 import shutil
 
 start_ts = time.time()
@@ -22,35 +17,23 @@ working_dir = ".sdkbuilder"
 
 def dump(fold, fw_name):
     fd = open(f'./System/Library/{fold}/{fw_name}.framework/{fw_name}', 'rb')
-    machofile = MachOFile(fd)
-    library = Dyld.load(machofile.slices[0])
-    objc_lib = ObjCImage(library)
     
-    
-    for objc_class in objc_lib.classlist:
-        objc_class.methods.sort(key=lambda h: h.signature)
-        objc_class.properties.sort(key=lambda h: h.name)
-        if objc_class.metaclass is not None:
-            objc_class.metaclass.methods.sort(key=lambda h: h.signature)
-
-    for objc_proto in objc_lib.protolist:
-        objc_proto.methods.sort(key=lambda h: h.signature)
-        objc_proto.opt_methods.sort(key=lambda h: h.signature)
-
-    hg = HeaderGenerator(objc_lib)
+    library = ktool.load_image(fd)
+    objc_lib = ktool.load_objc_metadata(library)
+        
     os.makedirs(f'{working_dir}/System/Library/{fold}/{fw_name}.framework', exist_ok=True)
     shutil.copyfile(f'./System/Library/{fold}/{fw_name}.framework/{fw_name}', f'{working_dir}/System/Library/{fold}/{fw_name}.framework/{fw_name}')
     
-    tbd_dict = TBDGenerator(library, True, objc_lib).dict
+    tbd_text = ktool.generate_text_based_stub(library, compatibility=True)
     with open(f'{working_dir}/System/Library/{fold}/{fw_name}.framework/{fw_name}.tbd', 'w') as tbd_out:
-        tbd_out.write(TapiYAMLWriter.write_out(tbd_dict))
+        tbd_out.write(tbd_text)
         
     os.makedirs(f'{working_dir}/System/Library/{fold}/{fw_name}.framework/Headers', exist_ok=True)
     
-    for header_name in hg.headers:
+    for header_name in ktool.generate_headers(objc_lib, sort_items=True):
         with open(f'{working_dir}/System/Library/{fold}/{fw_name}.framework/Headers' + '/' + header_name,
                   'w') as out:
-            out.write(str(hg.headers[header_name]))
+            out.write(hg.headers[header_name])
 
 
 def trydump(item):
