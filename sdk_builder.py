3702c1e06662592eb823940a7876b50d4ec261f3
kat <KritantaDevelopment@gmail.com>
2021-12-30 20:41:00 -0500

    Update sdk_builder.py

--- sdk_builder.py
+++ sdk_builder.py
@@ -17,7 +17,6 @@ working_dir = ".sdkbuilder"
 
 
 def dump(fold, fw_name):
-    opts.USE_SYMTAB_INSTEAD_OF_SELECTORS = True
     fd = open(f'./System/Library/{fold}/{fw_name}.framework/{fw_name}', 'rb')
     
     library = ktool.load_image(fd)
